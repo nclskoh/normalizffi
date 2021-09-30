27939009f86b636285e468fd2d8d0e06660fd4c3
Nicolas Koh <ckoh@princeton.edu>
2021-09-30 17:17:41 -0400

    copy bug in patch-offline.sh

--- Normaliz-patch/patch-offline.sh
+++ Normaliz-patch/patch-offline.sh
@@ -10,15 +10,15 @@ cp ./.gitignore ../Normaliz-offline/
 # The other files below are patched to get packages from this place instead of online.
 cp install_scripts_opt/common.sh ../Normaliz-offline/install_scripts_opt/common.sh
 
-cp install_scripts_opt/install_nmz_cocoa.sh ./Normaliz-offline/install_scripts_opt/install_nmz_cocoa.sh
+cp install_scripts_opt/install_nmz_cocoa.sh ../Normaliz-offline/install_scripts_opt/install_nmz_cocoa.sh
 
 # install_eantic_with_prerequisites.sh
-cp install_scripts_opt/install_nmz_mpfr.sh ./Normaliz-offline/install_scripts_opt/install_nmz_mpfr.sh
-cp install_scripts_opt/install_nmz_flint.sh ./Normaliz-offline/install_scripts_opt/install_nmz_flint.sh
-cp install_scripts_opt/install_nmz_arb.sh ./Normaliz-offline/install_scripts_opt/install_nmz_arb.sh
-cp install_scripts_opt/install_nmz_antic.sh ./Normaliz-offline/install_scripts_opt/install_nmz_antic.sh
-cp install_scripts_opt/install_nmz_e-antic.sh ./Normaliz-offline/install_scripts_opt/install_nmz_e-antic.sh
+cp install_scripts_opt/install_nmz_mpfr.sh ../Normaliz-offline/install_scripts_opt/install_nmz_mpfr.sh
+cp install_scripts_opt/install_nmz_flint.sh ../Normaliz-offline/install_scripts_opt/install_nmz_flint.sh
+cp install_scripts_opt/install_nmz_arb.sh ../Normaliz-offline/install_scripts_opt/install_nmz_arb.sh
+cp install_scripts_opt/install_nmz_antic.sh ../Normaliz-offline/install_scripts_opt/install_nmz_antic.sh
+cp install_scripts_opt/install_nmz_e-antic.sh ../Normaliz-offline/install_scripts_opt/install_nmz_e-antic.sh
 
-cp install_scripts_opt/install_nmz_nauty.sh ./Normaliz-offline/install_scripts_opt/install_nmz_nauty.sh
+cp install_scripts_opt/install_nmz_nauty.sh ../Normaliz-offline/install_scripts_opt/install_nmz_nauty.sh
 
 cp -r download ../Normaliz-offline/download
