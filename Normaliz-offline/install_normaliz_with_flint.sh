01cf2178a3582eb07e9bfe4f915fe1ef4b9b4f3a
Nicolas Koh <ckoh@princeton.edu>
2025-05-13 01:25:51 -0400

    Normaliz-offline downloads and installation script

---
+++ Normaliz-offline/install_normaliz_with_flint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+./install_scripts_opt/install_nmz_mpfr.sh
+./install_scripts_opt/install_nmz_flint.sh
+./install_scripts_opt/install_nmz_nauty.sh
+./install_normaliz.sh
